1e6a94f3393bb9f0210ff2759fb2c250bedc3195
TheBigMort <thebigmort1249@gmail.com>
2021-11-17 13:20:38 -0600

    small tweaks to router

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,6 @@ var express = require("express");
 var router = express.Router();
 const data = require("../resources/metadata.json");
 
-router.get("/clubcard/:req_id", (req, res) => {
-  let id = req.params.req_id;
-  if (id < 250) {
-    res.status(200).json(data[id - 1]);
-  } else {
-    res.status(200).json({
-      animation_url: `https://clubcards-web.s3.filebase.com/assets/pack${getRandomInt(
-        1,
-        3
-      )}.gif`,
-      description:
-        "Thank you for participating in the public sale! Wen reveal? Soon! CC 👊",
-    });
-  }
-});
-
 /* GET home page. */
 router.get("/", (req, res, next) => {
   res.render("index", { title: "Express" });
