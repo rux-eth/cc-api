43134b4027de3bf29978b71bc37d2861346ab4f9
TheBigMort <thebigmort1249@gmail.com>
2021-11-17 13:25:10 -0600

    small tweaks to router

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var router = express.Router();
-
+const Web3 = require("web3");
+const web3 = new Web3(
+  "https://mainnet.infura.io/v3/664fba9ff7364292afcb5373c6e9cc44"
+);
 /* GET home page. */
 router.get("/", (req, res, next) => {
   res.render("index", { title: "Express" });
