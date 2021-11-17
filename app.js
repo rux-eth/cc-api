91880895403c442aacc679a556d5f882060af639
TheBigMort <thebigmort1249@gmail.com>
2021-11-17 13:50:57 -0600

    small tweaks to router

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const abi = require("./resources/ClubCardsABI");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
