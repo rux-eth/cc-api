0ca67650af9cf68ba577207ec4aa078d8ced8da7
TheBigMort <thebigmort1249@gmail.com>
2021-11-19 05:23:16 -0600

    changed interval time

--- app.js
+++ app.js
@@ -26,15 +26,4 @@ app.use((req, res, next) => {
   res.sendStatus(400);
 });
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
 module.exports = app;
