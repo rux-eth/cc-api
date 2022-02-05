63a197a43a584b00daeb00f6f1bd618a65e8f49d
TheBigMort <thebigmort@Maxs-MacBook-Pro.local>
2022-02-04 20:43:24 -0600

    opt for heroku

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const indexRouter = require("./routes/index");
+const helmet = require("helmet");
 
 const app = express();
 
+app.use(helmet());
 app.use("/", indexRouter);
 app.use("/clubcard/:req_id", indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const helmet = require("helmet");
 const router = express.Router();
 const data = require("../resources/metadata.json");
 const abi = require("../resources/ClubCardsABI.json");
